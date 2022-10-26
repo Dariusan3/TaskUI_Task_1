ff65756e61111094fe5065bc96c632f8cb6e405e
Dariusan3 <darius.osadici@yahoo.ro>
2022-10-26 18:06:45 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,15 @@
+const img1 = document.querySelector('#imaginea_mea_1');
+const img2 = document.querySelector('#imaginea_mea_2');
+
+console.log("****")
+
+img1.addEventListener('click', ()=>{
+    img1.style.display = 'none';
+    img2.style.display = 'block';
+})
+
+img2.addEventListener('click', ()=> {
+    img1.style.display = 'block';
+    img2.style.display = 'none';
+})
+
